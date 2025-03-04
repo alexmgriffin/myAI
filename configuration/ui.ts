76ba97a0f0e0497a7d193f83e2fa54b60c142d64
Alexander Griffin <alexgriffin1@me.com>
2025-03-04 14:17:14 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,4 +1,4 @@
-import { Gameday_Guru, Alex_Griffin } from "@/configuration/identity";
+import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
 export const CHAT_HEADER: string = `[CHAT HEADER]`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
