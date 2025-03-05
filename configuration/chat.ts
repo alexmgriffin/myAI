ae70d7b1f1622209a4761bd83f94d91634ba87ea
Alexander Griffin <alexgriffin1@me.com>
2025-03-04 19:05:41 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -5,3 +5,4 @@ export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble gener
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `I'm reaching my response limit. Let's continue in a new message.`;
 export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+import { getBettingInsights } from "../configuration/bettingLogic";
