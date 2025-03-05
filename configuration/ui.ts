a935dde00cf9f111495d6ab9db40e809abcc0fca
Alexander Griffin <alexgriffin1@me.com>
2025-03-04 21:49:20 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,10 +1,10 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Gameday Guru - Beat the Odds`;
+export const CHAT_HEADER: string = `Meal Mate`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Customized AI by Alex Griffin]`;
-export const CLEAR_BUTTON_TEXT: string = `New Bet`;
-export const PAGE_TITLE: string = `GAMEDAY GURU`;
+export const CLEAR_BUTTON_TEXT: string = `New Meal`;
+export const PAGE_TITLE: string = `Meal Mate`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
