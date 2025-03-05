0cf3b80f6bc1b4a3198ec1c884695c3cc4eb87ac
Alexander Griffin <alexgriffin1@me.com>
2025-03-05 11:51:23 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -2,12 +2,12 @@ import { AI_NAME, OWNER_NAME } from "../configuration/identity";
 
 export const CHAT_HEADER: string = "Meal Mate";
 export const MESSAGE_PLACEHOLDER: string = "[MESSAGE PLACEHOLDER]";
-export const FOOTER_MESSAGE: string = `[Customized AI by ${OWNER_NAME}]`;
+export const FOOTER_MESSAGE: string = `Customized AI by ${OWNER_NAME}`;
 export const CLEAR_BUTTON_TEXT: string = "New Meal";
 export const PAGE_TITLE: string = "Meal Mate";
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
-export const BOT_AVATAR: string = "/CHatBotPic.png"; 
+export const BOT_AVATAR: string = 'CHatBotPic.png'; 
 
 // **Force UI Changes**
 export const CHAT_CONTAINER_STYLE = "bg-gray-200 min-h-screen flex flex-col items-center justify-center p-4"; 
