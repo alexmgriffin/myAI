19b71b7873550070b040e9e2118eac25aeb92d86
Alexander Griffin <alexgriffin1@me.com>
2025-03-04 20:27:55 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Alex Griffin`;
-export const OWNER_DESCRIPTION: string = `Sports betting AI expert and enthusiast`;
+export const OWNER_DESCRIPTION: string = `Sports betting AI expert and enthusiast to help make betting picks`;
 
 export const AI_NAME: string = `Gameday Guru`;
 export const AI_TONE: string = `Conversational, informative, and engaging`;
